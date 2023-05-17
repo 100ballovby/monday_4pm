d75e1febf7dd71e5c91952d974709f5bc377a29e
greatraksin <greatraksin@icloud.com>
2023-05-17 15:35:41 +0300

    Adding graph

--- main.cpp
+++ main.cpp
@@ -1,175 +1,180 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <ctime>
-#include <random>
 using namespace std;
 
-
-struct Graph {
-    vector<vector<int>> adj_matrix;
-    int num_vertices;
-    int num_edges;
+// A struct to represent a vertex
+struct Vertex {
+    int id;
+    vector<int> adjacentVertices;
 };
 
+// A struct to represent an edge
+struct Edge {
+    int sourceVertex;
+    int destinationVertex;
+};
 
-// функция записи графа в файл
-void writeGraphToFile(const Graph &graph, const string &filename) {
-    ofstream file(filename);
-    if (!file.is_open()) {
-        cout << "Error opening file!" << endl;
-        return;
-    }
-    file << graph.num_vertices << " " << graph.num_edges << endl;
-    for (int i = 0; i < graph.num_vertices; i++) {
-        for (int j = 0; j < graph.num_edges; j++) {
-            file << graph.adj_matrix[i][j] << " ";
+// A struct to represent a graph
+struct Graph {
+    vector<Vertex> vertices;
+    vector<Edge> edges;
+};
+
+// A function to print the graph data to a text file
+void printGraphToFile(const Graph &graph, const string &filename) {
+    ofstream outfile(filename);
+
+    // Print the number of vertices
+    outfile << graph.vertices.size() << endl;
+
+    // Print the vertices
+    for (int i = 0; i < graph.vertices.size(); i++) {
+        outfile << graph.vertices[i].id << " ";
+        for (int j = 0; j < graph.vertices[i].adjacentVertices.size(); j++) {
+            outfile << graph.vertices[i].adjacentVertices[j] << " ";
         }
-        file << endl;
+        outfile << endl;
     }
-    file.close();
+
+    // Print the edges
+    for (int i = 0; i < graph.edges.size(); i++) {
+        outfile << graph.edges[i].sourceVertex << " " << graph.edges[i].destinationVertex << endl;
+    }
+
+    outfile.close();
 }
 
-// чтение графа из файла
+// A function to read the graph data from a text file
 Graph readGraphFromFile(const string &filename) {
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cout << "Error opening file!" << endl;
-        return {{}, 0, 0};
-    }
-    int num_vertices, num_edges;
-    file >> num_vertices >> num_edges;
-    vector<vector<int>> adj_matrix(num_vertices, vector<int>(num_vertices));
+    Graph graph;
 
-    for (int i = 0; i < num_vertices; i++) {
-        for (int j = 0; j < num_vertices; j++) {
-            file >> adj_matrix[i][j];
-        }
+    ifstream infile(filename);
+
+    // Read the number of vertices
+    int numVertices;
+    infile >> numVertices;
+
+    // Read the vertices
+    for (int i = 0; i < numVertices; i++) {
+        Vertex vertex;
+        infile >> vertex.id;
+        graph.vertices.push_back(vertex);
     }
-    file.close();
-    return { adj_matrix, num_vertices, num_edges};
+
+    // Read the edges
+    for (int i = 0; i < numVertices; i++) {
+        Edge edge;
+        infile >> edge.sourceVertex >> edge.destinationVertex;
+        graph.edges.push_back(edge);
+    }
+
+    infile.close();
+
+    return graph;
 }
 
+// A function to fill the graph randomly by asking the user for the number of vertices and edges
+void fillGraphRandomly(Graph &graph) {
+    int numVertices, numEdges;
 
-// функция заполнения графа случайными числами
-void generateRandomGraph(Graph &graph) {
-    default_random_engine generator;
-    uniform_int_distribution<int> distribution(0, 1);
-    cout << "Num of vertices: ";
-    cin >> graph.num_vertices;
-    graph.adj_matrix.resize(graph.num_vertices);
-    for (int i = 0; i < graph.num_vertices; i++) {
-        graph.adj_matrix[i].resize(graph.num_vertices);
-        for (int j = 0; j < i; j++) {
-            int val = distribution(generator);
-            graph.adj_matrix[i][j] = graph.adj_matrix[j][i] = val;
-            if (val == 1) {
-                graph.num_edges++;
-            }
-        }
+    // Ask the user for the number of vertices
+    cout << "Enter the number of vertices: ";
+    cin >> numVertices;
+
+    // Ask the user for the number of edges
+    cout << "Enter the number of edges: ";
+    cin >> numEdges;
+
+    // Create the vertices
+    for (int i = 0; i < numVertices; i++) {
+        Vertex vertex;
+        vertex.id = i;
+        graph.vertices.push_back(vertex);
+    }
+
+    // Create the edges
+    for (int i = 0; i < numEdges; i++) {
+        Edge edge;
+        edge.sourceVertex = rand() % numVertices;
+        edge.destinationVertex = rand() % numVertices;
+        graph.edges.push_back(edge);
     }
 }
 
+// A function that allows using the given adjacency matrix (data set from the console) to build an incidence matrix
+void buildIncidenceMatrix(const Graph &graph, vector<vector<int>> &incidenceMatrix) {
+    int numVertices = graph.vertices.size();
+    int numEdges = graph.edges.size();
 
-// функция для построения матрицы инцидентности на основе матрицы смежности
-vector<vector<int>> getIncidentMatrix(const vector<vector<int>> &adj_matrix) {
-    int num_vertices = adj_matrix.size();
-    int num_edges = 0;
-    for (int i = 0; i < num_vertices; i++) {
-        for (int j = i + 1; j < num_vertices; j++) {
-            if (adj_matrix[i][j] == 1) {
-                num_edges++;
-            }
-        }
-    }
+    // Create the incidence matrix
+    incidenceMatrix.resize(numVertices);
+    // Iterate over the edges
+    for (int i = 0; i < numEdges; i++) {
+        // Get the source and destination vertices
+        int sourceVertex = graph.edges[i].sourceVertex;
+        int destinationVertex = graph.edges[i].destinationVertex;
 
-    vector<vector<int>> incidentMatrix (num_vertices, vector<int>(num_edges));
-    int edge_index = 0;
-    for (int i = 0; i < num_vertices; i++) {
-        for (int j = i + 1; j < num_vertices; j++) {
-            if (adj_matrix[i][j] == 1) {
-                incidentMatrix[i][edge_index] = incidentMatrix[j][edge_index] = 1;
-                edge_index++;
-            }
-        }
+        // Set the corresponding entries in the incidence matrix
+        incidenceMatrix[sourceVertex].push_back(i);
+        incidenceMatrix[destinationVertex].push_back(i);
     }
-    return incidentMatrix;
 }
 
-
+// The function of adding a vertex. Data about incident edges is set from the console.
 void addVertex(Graph &graph) {
-    int new_vertex = graph.num_vertices;
-    graph.num_vertices++;
-    graph.adj_matrix.push_back(vector<int>(graph.num_vertices));
-    for (int i = 0; i < graph.num_vertices; i++) {
-        graph.adj_matrix[i].push_back(0);
-    }
-    cout << "Enter nums of vertices connected with new vertex (from 0 to " << new_vertex - 1 << "): " << endl;
-    for (int i = 0; i < graph.num_vertices; i++) {
-        int connected;
-        cout << "Vertex " << i << ": ";
-        cin >> connected;
+    int vertexId;
 
-        if (connected >= 0 && connected < new_vertex) {
-            graph.adj_matrix[new_vertex][connected] = graph.adj_matrix[connected][new_vertex] = 1;
-            graph.num_edges++;
-        } else {
-            cout << "Incorrect number of vertex!" << endl;
-            i--;
-        }
+    // Ask the user for the vertex id
+    cout << "Enter the vertex id: ";
+    cin >> vertexId;
+
+    // Create the vertex
+    Vertex vertex;
+    vertex.id = vertexId;
+    graph.vertices.push_back(vertex);
+
+    // Ask the user for the number of incident edges
+    int numIncidentEdges;
+    cout << "Enter the number of incident edges: ";
+    cin >> numIncidentEdges;
+
+    // Create the incident edges
+    for (int i = 0; i < numIncidentEdges; i++) {
+        Edge edge;
+        edge.sourceVertex = vertexId;
+        edge.destinationVertex = rand() % graph.vertices.size();
+        graph.edges.push_back(edge);
     }
 }
-
-// функция для получения всех цепей, исходящих из заданной вершины
-void getPathsFromVertex(const Graph &graph, int vertex, vector<int> &path, vector<vector<int>> &paths) {
-    path.push_back(vertex);
-    if (path.size() == graph.num_vertices) {
-        paths.push_back(path);
-    } else {
-        for (int i = 0; i < graph.num_vertices; i++) {
-            if (graph.adj_matrix[vertex][i] == 1 && find(path.begin(), path.end(), i) == path.end()) {
-                getPathsFromVertex(graph, i, path, paths);
-            }
-        }
-    }
-    path.pop_back();
-}
-
 
 int main() {
-    Graph graph = { {}, 0, 0 };
+    // Create a graph
+    Graph graph;
 
-    while (true) {
-        cout << "Choose action: " << endl;
-        cout << "1. Записать граф в файл." << endl;
-        cout << "2. Прочитать граф из файла." << endl;
-        cout << "3. Заполнить граф случайными числами." << endl;
-        cout << "4. Построить матрицу инцидентности." << endl;
-        cout << "5. Добавить вершину." << endl;
-        cout << "6. Выйти.";
-        int action;
-        cin >> action;
+    // Fill the graph randomly
+    fillGraphRandomly(graph);
 
-        if (action == 1) {
-            writeGraphToFile(graph, "../graph.txt");
-        } else if (action == 2) {
-            Graph graph2;
-            graph2 = readGraphFromFile("../graph.txt");
-        } else if (action == 3) {
-            generateRandomGraph(graph);
-        } else if (action == 4) {
-            vector<vector<int>> incident = getIncidentMatrix(graph.adj_matrix);
-            for (int i = 0; i < incident.size(); i++) {
-                for (int j = 0; i < incident[i].size(); j++) {
-                    cout << incident[i][j] << " ";
-                }
-                cout << endl;
-            }
-        } else if (action == 5) {
-            addVertex(graph);
-        } else if (action == 6) {
-            break;
+    // Print the graph data to a text file
+    printGraphToFile(graph, "../graph.txt");
+
+    // Read the graph data from a text file
+    Graph graph2 = readGraphFromFile("../graph.txt");
+
+    // Build the incidence matrix
+    vector<vector<int>> incidenceMatrix;
+    buildIncidenceMatrix(graph2, incidenceMatrix);
+
+    // Print the incidence matrix
+    for (int i = 0; i < incidenceMatrix.size(); i++) {
+        for (int j = 0; j < incidenceMatrix[i].size(); j++) {
+            cout << incidenceMatrix[i][j] << " ";
         }
+        cout << endl;
     }
+
+    // Add a vertex
+    addVertex(graph2);
+
     return 0;
 }
